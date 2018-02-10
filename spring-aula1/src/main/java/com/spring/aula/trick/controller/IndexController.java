fd8088168643942109313419fb54494bc6727fd2
Patrick da Silva <patrickeeep12@gmail.com>
2018-02-10 14:19:23 -0300

    Criando métodos raiz

--- spring-aula1/src/main/java/com/spring/aula/trick/controller/IndexController.java
+++ spring-aula1/src/main/java/com/spring/aula/trick/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.spring.aula.trick.controller;
 
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,11 @@ public class IndexController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String getTexto(){
         return "Olá pessoal!";
+    }
+
+    @RequestMapping(value = "/{nome}", method = RequestMethod.GET)
+    public String getTexto(@PathVariable String nome){
+        return "Olá pessoal, sou o " + nome;
     }
 
 }
