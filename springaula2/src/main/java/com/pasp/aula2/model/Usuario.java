08aa8ececcf29a7add5df930a653dd8575d3241d
Patrick da Silva <patrickeeep12@gmail.com>
2018-02-10 17:09:08 -0300

    Criando métodos do usuarioservice, criando atributos no usuario, criando método listausuario

--- springaula2/src/main/java/com/pasp/aula2/model/Usuario.java
+++ springaula2/src/main/java/com/pasp/aula2/model/Usuario.java
@@ -3,6 +3,17 @@ package com.pasp.aula2.model;
 public class Usuario {
 
     private String nome;
+    private String email;
+    private int idade;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
 
     public String getNome() {
         return nome;
@@ -10,5 +21,21 @@ public class Usuario {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
     }
 }
--- springaula2/src/main/java/com/pasp/aula2/service/UsuarioService.java
+++ springaula2/src/main/java/com/pasp/aula2/service/UsuarioService.java
@@ -1,10 +1,33 @@
 package com.pasp.aula2.service;
 
+import com.pasp.aula2.model.Usuario;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UsuarioService {
 
+    public List<Usuario> listaUsuario(){
+        List<Usuario> usuarioLista = new ArrayList<Usuario>();
 
+        usuarioLista.add(new Usuario("Patrick", "patrick@gmail.com", 21));
+        usuarioLista.add(new Usuario("Gabriel", "gabriel@gmail.com",20));
+        usuarioLista.add(new Usuario("Bruno", "bruno@gmail.com", 19));
+
+        return usuarioLista;
+    }
+
+    public List<Usuario> listaUsuario(Usuario usuario){
+        List<Usuario> usuarioLista = new ArrayList<Usuario>();
+
+        usuarioLista.add(new Usuario("Patrick", "patrick@gmail.com", 21));
+        usuarioLista.add(new Usuario("Gabriel", "gabriel@gmail.com",20));
+        usuarioLista.add(new Usuario("Bruno", "bruno@gmail.com", 19));
+        usuarioLista.add(usuario);
+
+        return usuarioLista;
+    }
 
 }
