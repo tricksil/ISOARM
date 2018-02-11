e10bf7834b28cd058534f591c4c6157284412da9
Patrick da Silva <patrickeeep12@gmail.com>
2018-02-11 09:12:24 -0300

    Projeto 2

--- spring-aula2/src/main/java/com/pasp/aula2/controller/IndexController.java
+++ spring-aula2/src/main/java/com/pasp/aula2/controller/IndexController.java
@@ -1,12 +1,9 @@
 package com.pasp.aula2.controller;
 
 
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.pasp.aula2.model.Usuario;
 
 @RestController
 public class IndexController {
