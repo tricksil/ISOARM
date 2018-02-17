2a361d83ecc8bea02bcea918bf93701645b59835
Patrick Silva <patrickeeep12@gmail.com>
2018-02-17 07:46:32 -0300

    Criando construtor e atributo senha

--- spring-aula7/src/main/java/com/pasp/model/Usuario.java
+++ spring-aula7/src/main/java/com/pasp/model/Usuario.java
@@ -16,18 +16,20 @@ public class Usuario {
     
     private String email;
     
+    private String senha;
+    
     private int idade;
     
     @DBRef
     private List<Perfil> perfis;
 
-    public Usuario() {
-    }
-
-    public Usuario(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
+    public Usuario(){}
+    
+    public Usuario(Usuario usuario) {
+    	this.nome = usuario.getNome();
+    	this.email = usuario.getEmail();
+    	this.senha = usuario.getSenha();
+    	this.perfis = usuario.getPerfis();
     }
     
     public String getId() {
@@ -52,9 +54,17 @@ public class Usuario {
 
     public void setEmail(String email) {
         this.email = email;
-    }
+    }    
+    
+    public String getSenha() {
+		return senha;
+	}
 
-    public int getIdade() {
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public int getIdade() {
         return idade;
     }
 
