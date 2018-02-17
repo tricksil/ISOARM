cf8c55abe6b7717143998d968073616fd314ea2a
Patrick Silva <patrickeeep12@gmail.com>
2018-02-17 08:07:56 -0300

    Criando Bean para autenticação

--- spring-aula7/src/main/java/com/pasp/config/WebSecurityConfiguration.java
+++ spring-aula7/src/main/java/com/pasp/config/WebSecurityConfiguration.java
@@ -1,7 +1,9 @@
 package com.pasp.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,6 +19,11 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(myUserDetailService);
+	}
+	
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception{
+		return super.authenticationManagerBean();
 	}
 	
 }
