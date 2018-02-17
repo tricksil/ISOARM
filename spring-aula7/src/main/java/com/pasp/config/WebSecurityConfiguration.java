2948ad21fa55a8a9e795bb48b961728985b3c171
Patrick Silva <patrickeeep12@gmail.com>
2018-02-17 08:22:10 -0300

    Configurando WebSecurity

--- spring-aula7/src/main/java/com/pasp/config/WebSecurityConfiguration.java
+++ spring-aula7/src/main/java/com/pasp/config/WebSecurityConfiguration.java
@@ -3,8 +3,10 @@ package com.pasp.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -24,6 +26,12 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Bean
 	public AuthenticationManager authenticationManagerBean() throws Exception{
 		return super.authenticationManagerBean();
+	}
+	
+	public void configure (WebSecurity security) throws Exception{
+		security.ignoring()
+					.antMatchers(HttpMethod.OPTIONS, "/**")
+				.antMatchers(HttpMethod.GET, "/public/**");
 	}
 	
 }
