b815a90f0e8640c0aa33026453651d95e73d1ff7
Patrick Silva <patrickeeep12@gmail.com>
2018-02-17 08:54:58 -0300

    Configurando defaulttokenservices

--- spring-aula7/src/main/java/com/pasp/config/AuthorizationServerConfiguration.java
+++ spring-aula7/src/main/java/com/pasp/config/AuthorizationServerConfiguration.java
@@ -2,12 +2,15 @@ package com.pasp.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
@@ -41,6 +44,16 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
 				.refreshTokenValiditySeconds(20000)
 				.accessTokenValiditySeconds(20000)
 				.secret("123");
+	}
+	
+	@Bean
+	@Primary
+	public DefaultTokenServices tokenServices() {
+		DefaultTokenServices  tokenServices  = new DefaultTokenServices();
+		
+		tokenServices.setSupportRefreshToken(true);
+		tokenServices.setTokenStore(tokenStore);
+		return tokenServices;
 	}
 	
 }
