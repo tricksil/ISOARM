1fd021f855f28f0ef9997154382498af0379128e
Patrick Silva <patrickeeep12@gmail.com>
2018-02-17 09:45:21 -0300

    Configurando resource

--- spring-aula7/src/main/java/com/pasp/config/ResourceServerConfiguration.java
+++ spring-aula7/src/main/java/com/pasp/config/ResourceServerConfiguration.java
@@ -5,10 +5,15 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+	
+	public void configure (ResourceServerSecurityConfigurer resource) {
+		resource.resourceId("restservice");
+	}
 	
 	public void configure (HttpSecurity http) throws Exception{
 		http	
