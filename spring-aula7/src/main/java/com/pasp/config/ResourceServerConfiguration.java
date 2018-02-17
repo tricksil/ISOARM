5ad4af0a0157f14a33e1427fc127fa6633b1f6ca
Patrick Silva <patrickeeep12@gmail.com>
2018-02-17 09:05:39 -0300

    Configurando HttpSecurity

---
+++ spring-aula7/src/main/java/com/pasp/config/ResourceServerConfiguration.java
@@ -0,0 +1,22 @@
+package com.pasp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+	
+	public void configure (HttpSecurity http) throws Exception{
+		http	
+				.logout()
+				.invalidateHttpSession(true)
+				.clearAuthentication(true)
+				.and().authorizeRequests()
+				.antMatchers("usuario/**").hasAnyRole("ADMIN")
+				.anyRequest().denyAll();
+	}
+	
+}
