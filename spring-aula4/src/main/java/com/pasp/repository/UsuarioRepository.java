0e65f7d09e4d5d64671d5e4dbcbf4a449f14d3d2
Patrick Silva <patrickeeep12@gmail.com>
2018-02-17 12:37:44 -0300

    removendo spring-aula4

--- spring-aula4/src/main/java/com/pasp/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pasp.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.pasp.model.Usuario;
-
-public interface UsuarioRepository extends MongoRepository<Usuario, String>{
-
-}
