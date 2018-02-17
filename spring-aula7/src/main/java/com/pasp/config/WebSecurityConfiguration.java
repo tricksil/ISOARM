0dd3493231bd996c995f9a97ce83958be909e756
Patrick Silva <patrickeeep12@gmail.com>
2018-02-17 07:25:59 -0300

    Criando pacote e classe para configuração

---
+++ spring-aula7/src/main/java/com/pasp/config/WebSecurityConfiguration.java
@@ -0,0 +1,11 @@
+package com.pasp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+}
