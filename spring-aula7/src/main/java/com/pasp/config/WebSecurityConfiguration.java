d0dbccac81f1409aa9f170e7d4c92b293a3e8004
Patrick Silva <patrickeeep12@gmail.com>
2018-02-17 08:05:01 -0300

    Criando método configure

--- spring-aula7/src/main/java/com/pasp/config/WebSecurityConfiguration.java
+++ spring-aula7/src/main/java/com/pasp/config/WebSecurityConfiguration.java
@@ -1,11 +1,22 @@
 package com.pasp.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import com.pasp.service.MyUserDetailService;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
-
+	
+	@Autowired
+	private MyUserDetailService myUserDetailService;
+	
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(myUserDetailService);
+	}
+	
 }
