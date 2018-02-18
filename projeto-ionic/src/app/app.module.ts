b7971e9bec7717d18dca67033601457ef7230f8b
Patrick Silva <patrickeeep12@gmail.com>
2018-02-18 05:56:40 -0300

    Adicionando o HttpModule

--- projeto-ionic/src/app/app.module.ts
+++ projeto-ionic/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule, ErrorHandler } from '@angular/core';
+import { Http, HttpModule } from '@angular/http'
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
@@ -22,6 +23,7 @@ import { LoginServiceProvider } from '../providers/login-service/login-service';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
