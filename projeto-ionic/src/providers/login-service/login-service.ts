3d31ca26d34ea97df098bcf5a6079ed4e9feb6b2
Patrick Silva <patrickeeep12@gmail.com>
2018-02-18 05:47:07 -0300

    Criando metodo login

--- projeto-ionic/src/providers/login-service/login-service.ts
+++ projeto-ionic/src/providers/login-service/login-service.ts
@@ -1,5 +1,13 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Headers, Http, RequestOptions } from '@angular/http';
+
+
+import { Observable } from 'rxjs';
+
+import 'rxjs/add/operator/map';
+
+import { Utils } from '../../model/utils';
+import { Usuario } from '../../model/usuario';
 
 /*
   Generated class for the LoginServiceProvider provider.
@@ -10,8 +18,24 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class LoginServiceProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello LoginServiceProvider Provider');
+  private loginUrl: string;
+  public handleError: any;
+
+  constructor(public http: Http) {
+    this.loginUrl = Utils.getUrlBackend() + "oauth/token?grant_type=password&username="
+  }
+
+  public login(usuario: Usuario): Observable<any>{
+    this.loginUrl + usuario.email + "&password=" + encodeURIComponent(usuario.senha);
+    let headers = new Headers({
+      "Authorization": "Basic " + btoa("cliente"+ ':' + "123")
+    });
+
+    let options = new RequestOptions({ headers: headers });
+
+    return this.http.post(this.loginUrl + usuario.email + "&password=" +
+    encodeURIComponent(usuario.senha), {}, options)
+    .map(res => res.json());
   }
 
 }
