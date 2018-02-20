4360f33eca4a97cd6c6a48dd7319b953bb838e4e
Patrick Silva <patrickeeep12@gmail.com>
2018-02-20 06:53:27 -0300

    Ver se existe usuario logado

--- projeto-ionic/src/app/app.component.ts
+++ projeto-ionic/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { LoginPage } from '../pages/login/login';
 import { TabsPage } from '../pages/tabs/tabs';
+import { CookieService } from 'angular2-cookie/core';
 
 @Component({
   templateUrl: 'app.html'
@@ -14,9 +15,18 @@ export class MyApp {
 
   rootPage:any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    private cookieService: CookieService
+  ) {
+    if(this.cookieService.getObject("usuarioAtual")){
+      this.rootPage = TabsPage;
+    } else {
+      this.rootPage = LoginPage;
+    }
 
-    this.rootPage = LoginPage;
 
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
