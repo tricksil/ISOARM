11dbd4942c29f02beca062a2b5f54333b954002c
Patrick Silva <patrickeeep12@gmail.com>
2018-02-20 12:09:31 -0300

    Consertando imports

--- projeto-ionic/src/providers/interceptor-http.service.ts
+++ projeto-ionic/src/providers/interceptor-http.service.ts
@@ -1,21 +1,28 @@
 import { Injectable } from "@angular/core";
-import { Http, ConnectionBackend, Request, RequestOptions, RequestOptionsArgs, Response } from "@angular/http";
+import {
+  Http,
+  ConnectionBackend,
+  RequestOptions,
+  Request,
+  Response,
+  RequestOptionsArgs
+} from "@angular/http";
 import { Observable } from "rxjs/Observable";
-
 
 @Injectable()
 export class InterceptorHttpService extends Http {
-
-  constructor(backend: ConnectionBackend, defaultOptions: RequestOptions){
+  constructor(backend: ConnectionBackend, defaultOptions: RequestOptions) {
     super(backend, defaultOptions);
   }
 
-  request(url: string | Request, options?: RequestOptionsArgs): Observable<Response> {
-      return super.request(url, options);
+  request(
+    url: string | Request,
+    options?: RequestOptionsArgs
+  ): Observable<Response> {
+    return super.request(url, options);
   }
 
   get(url: string, options?: Request): Observable<Response> {
-      return super.get(url,options);
+    return super.get(url, options);
   }
-
 }
