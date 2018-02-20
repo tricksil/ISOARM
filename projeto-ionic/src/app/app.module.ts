5aba1602083341a4470b2031db07bbb9e94b5c27
Patrick Silva <patrickeeep12@gmail.com>
2018-02-20 08:22:27 -0300

    Importando Utils para app.module

--- projeto-ionic/src/app/app.module.ts
+++ projeto-ionic/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { LoginServiceProvider } from '../providers/login-service/login-service';
+import { Utils } from '../model/utils';
 
 
 @NgModule({
@@ -42,6 +43,7 @@ import { LoginServiceProvider } from '../providers/login-service/login-service';
     CookieService,
     StatusBar,
     SplashScreen,
+    Utils,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     LoginServiceProvider
   ]
